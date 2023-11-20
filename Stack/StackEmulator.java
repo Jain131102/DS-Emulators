d5f61c32c1b5ad185b648a91aa5638ce8fe7e588
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2023-11-20 19:18:33 +0530

    Update StackEmulator.java

--- Stack/StackEmulator.java
+++ Stack/StackEmulator.java
@@ -1,42 +1,59 @@
+
+// Import necessary classes and packages from JavaFX
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.shape.Polygon;
 import javafx.geometry.Pos;
+// Import Stack class from Java's util package
 import java.util.Stack;
+// Import Insets class from JavaFX geometry package
 import javafx.geometry.Insets;
+// Import Font and FontWeight classes from JavaFX text package
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.application.Platform; // Added import for Platform
-
-import javafx.animation.TranslateTransition;
-import javafx.animation.RotateTransition;
+// Import Platform class from JavaFX application package
+import javafx.application.Platform;
+// Import FadeTransition and Duration classes from JavaFX animation package
 import javafx.animation.FadeTransition;
 import javafx.util.Duration;
+// Import ScaleTransition class from JavaFX animation package
 import javafx.animation.ScaleTransition;
 
 public class StackEmulator extends Application {
+    // Declare a private Stack to represent the stack data structure with Integer
+    // elements
     private Stack<Integer> stack = new Stack<>();
+
+    // Declare a private integer variable to store the size of the array
     private int arraySize = 0;
+
+    // Declare a private VBox (Vertical Box) to visually organize UI elements
+    // related to the stack
     private VBox stackBox = new VBox(10);
-    private  Button setSizeButton,
-    pushButton,
-    peekButton,
-    popButton,
-    clearButton,
-    reverseButton,
-    learnButton,
-    exitButton;
-    ToggleButton toggleDarkModeButton;
+
+    // Declare private buttons for various stack operations: setSize, push, peek,
+    // pop, clear, reverse, learn, and exit
+    private Button setSizeButton, pushButton, peekButton, popButton, clearButton, reverseButton, learnButton,
+            exitButton;
+
+    // Declare a private ToggleButton to toggle between dark and light modes in the
+    // application
+    private ToggleButton toggleDarkModeButton;
+
+    // The main method, starting point for the JavaFX application
     public static void main(String[] args) {
+        // Launch the JavaFX application by calling the launch method with the provided
+        // arguments
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle(" Stack Emulator ");
+
+        // Set the title of the primary stage
+        primaryStage.setTitle("Stack Emulator");
 
         // Create UI elements
         TextField elementInput = new TextField();
@@ -45,9 +62,11 @@ public class StackEmulator extends Application {
         TextField sizeInput = new TextField();
         sizeInput.setPromptText("Enter array size");
 
+        // Add "User Guide" button
         Button startButton = new Button("User Guide");
         startButton.setOnAction(e -> showGuide());
 
+        // Initialize buttons for various stack operations
         setSizeButton = new Button("Set Size");
         setSizeButton.setOnAction(e -> setArraySize(sizeInput.getText()));
 
@@ -83,6 +102,7 @@ public class StackEmulator extends Application {
         reverseButton.setStyle("-fx-base: #800080;"); // Purple
         learnButton.setStyle("-fx-base: #4B0082;"); // Dark Blue for "Learn" button
         exitButton.setStyle("-fx-base: #e74c3c;"); // Red for "Exit" button
+
         // Add tooltips
         setSizeButton.setTooltip(new Tooltip("Set Array Size"));
         pushButton.setTooltip(new Tooltip("Push Element"));
@@ -101,11 +121,13 @@ public class StackEmulator extends Application {
         stackBox.setMaxWidth(200);
         HBox buttonBox = new HBox(10); // Horizontal container for buttons
         buttonBox.setAlignment(Pos.CENTER); // Center-align the buttons horizontally
-        buttonBox.getChildren().addAll(peekButton, popButton, clearButton, reverseButton, learnButton); // Added "Learn" button
+        buttonBox.getChildren().addAll(peekButton, popButton, clearButton, reverseButton, learnButton); // Added "Learn"
+                                                                                                        // button
 
         HBox inputBox = new HBox(10); // Horizontal container for input elements
         inputBox.setAlignment(Pos.CENTER); // Center-align the input elements
-        inputBox.getChildren().addAll(setSizeButton, sizeInput); // "Set Size" button and size input on the same line, "Push" button and element input on the same line
+        inputBox.getChildren().addAll(setSizeButton, sizeInput); // "Set Size" button and size input on the same line,
+                                                                 // "Push" button and element input on the same line
 
         HBox pushBox = new HBox(10);
         pushBox.setAlignment(Pos.CENTER);
@@ -118,9 +140,8 @@ public class StackEmulator extends Application {
         toggleDarkModeButton = new ToggleButton("Dark Mode");
         toggleDarkModeButton.setOnAction(e -> toggleDarkMode(layout));
         toggleDarkModeButton.setStyle("-fx-base: #34495e;"); // Set the initial style for the button
-        layout.getChildren().addAll(startButton, inputBox, pushBox, buttonBox, stackBox, exitBox,toggleDarkModeButton); // Added exitBox
+        layout.getChildren().addAll(startButton, inputBox, pushBox, buttonBox, stackBox, exitBox, toggleDarkModeButton);
         layout.setStyle("-fx-background-color: #2b2b2b;"); // Set the background color for the entire scene
-        // Add "Toggle Dark Mode" button
 
         Scene scene = new Scene(layout, 600, 600);
         primaryStage.setScene(scene);
@@ -132,7 +153,7 @@ public class StackEmulator extends Application {
         if (toggleDarkModeButton.isSelected()) {
             // Dark mode
             layout.setStyle("-fx-background-color: #2b2b2b;");
-            //   setButtonStyle("-fx-base: #34495e;"); // Set button color in dark mode
+            // setButtonStyle("-fx-base: #34495e;"); // Set button color in dark mode
         } else {
             // Light mode
             layout.setStyle("-fx-background-color: #ffffff;");
@@ -141,77 +162,105 @@ public class StackEmulator extends Application {
     }
 
     private void showLearnText() {
+        // Create a new stage for learning about stacks
         Stage learnStage = new Stage();
         learnStage.setTitle("Learn about Stacks");
 
+        // Create a layout for the learning stage
         VBox learnLayout = new VBox(10);
         learnLayout.setAlignment(Pos.CENTER);
         learnLayout.setPadding(new Insets(20));
 
         // Learn messages
-        String learnText = "What is Stack ? \n\n"+"Stack is a linear data structure that follows a particular order in which the operations are performed. The order may be LIFO (Last In First Out) or FILO (First In Last Out). LIFO implies that the element that is inserted last comes out first, and FILO implies that the element that is inserted first comes out last.\n\n"
-            + "There are many real-life examples of a stack. Consider an example of plates stacked over one another in the canteen. The plate which is at the top is the first one to be removed, i.e., the plate which has been placed at the bottommost position remains in the stack for the longest period of time. So, it can be simply seen to follow LIFO (Last In First Out)/FILO (First In Last Out) order.";
+        String learnText = "What is Stack ? \n\n" +
+                "Stack is a linear data structure that follows a particular order in which the operations are performed. "
+                +
+                "The order may be LIFO (Last In First Out) or FILO (First In Last Out). LIFO implies that the element that "
+                +
+                "is inserted last comes out first, and FILO implies that the element that is inserted first comes out last.\n\n"
+                +
+                "There are many real-life examples of a stack. Consider an example of plates stacked over one another in the "
+                +
+                "canteen. The plate which is at the top is the first one to be removed, i.e., the plate which has been placed "
+                +
+                "at the bottommost position remains in the stack for the longest period of time. So, it can be simply seen "
+                +
+                "to follow LIFO (Last In First Out)/FILO (First In Last Out) order.";
 
         Label learnLabel = new Label(learnText);
         learnLabel.setFont(Font.font("Arial", FontWeight.BOLD, 14));
         learnLabel.setWrapText(true);
 
+        // Create an exit button to close the learning stage
         Button exitButton = new Button("Exit");
         exitButton.setOnAction(e -> learnStage.close()); // Close only the learnStage
         exitButton.setStyle("-fx-base: #e74c3c;"); // Red for "Exit" button
         exitButton.setTooltip(new Tooltip("Exit Learning"));
 
+        // Create an HBox for the exit button at the bottom right
         HBox exitBox = new HBox(10);
         exitBox.setAlignment(Pos.BOTTOM_RIGHT); // Position at the bottom right
         exitBox.getChildren().addAll(exitButton);
 
+        // Add components to the learning layout
         learnLayout.getChildren().addAll(learnLabel, exitBox);
 
+        // Create a scene for the learning stage
         Scene learnScene = new Scene(learnLayout, 500, 500);
         learnStage.setScene(learnScene);
         learnStage.show();
     }
 
+    // Function to display the user guide
     private void showGuide() {
+        // Create a new stage for the user guide
         Stage guideStage = new Stage();
         guideStage.setTitle("User Guide");
 
+        // Create a layout for the user guide stage
         VBox guideLayout = new VBox(10);
         guideLayout.setAlignment(Pos.CENTER);
         guideLayout.setPadding(new Insets(20));
 
         // Guide messages
-        String messages = "\tStack Emulator\n\n"+
-            "This is a virtual environment or simulation where users can interact with and observe the operations of a stack data structure .\n\n"+
-            "Follow the steps below to explore the functionalities :\n\n"+
-            "1. Set Size : Set the size of the array.\n"+
-            "2. Push : Add an element to the stack.\n"+
-            "3. Peek : View the top element without removing it.\n"+
-            "4. Pop : Remove the top element from the stack.\n"+
-            "5. Clear Stack : Remove all elements from the stack.\n"+
-            "6. Reverse Stack : Reverse the order of elements in the stack.\n\n"+
-            "Feel free to explore and use these functionalities!";
+        String messages = "\tStack Emulator\n\n" +
+                "This is a virtual environment or simulation where users can interact with and observe the operations of a "
+                +
+                "stack data structure.\n\n" +
+                "Follow the steps below to explore the functionalities :\n\n" +
+                "1. Set Size : Set the size of the array.\n" +
+                "2. Push : Add an element to the stack.\n" +
+                "3. Peek : View the top element without removing it.\n" +
+                "4. Pop : Remove the top element from the stack.\n" +
+                "5. Clear Stack : Remove all elements from the stack.\n" +
+                "6. Reverse Stack : Reverse the order of elements in the stack.\n\n" +
+                "Feel free to explore and use these functionalities!";
 
         Label label = new Label(messages);
         label.setFont(Font.font("Arial", FontWeight.BOLD, 14));
         label.setWrapText(true);
 
+        // Create an exit button to close the user guide stage
         Button exitButton = new Button("Exit");
         exitButton.setOnAction(e -> guideStage.close()); // Close only the guideStage
         exitButton.setStyle("-fx-base: #e74c3c;"); // Red for "Exit" button
         exitButton.setTooltip(new Tooltip("Exit Guide"));
 
+        // Create an HBox for the exit button at the bottom right
         HBox exitBox = new HBox(10);
         exitBox.setAlignment(Pos.BOTTOM_RIGHT); // Position at the bottom right
         exitBox.getChildren().addAll(exitButton);
 
+        // Add components to the user guide layout
         guideLayout.getChildren().addAll(label, exitBox);
 
+        // Create a scene for the user guide stage
         Scene guideScene = new Scene(guideLayout, 500, 500);
         guideStage.setScene(guideScene);
         guideStage.show();
     }
 
+    // Function to set the size of the array
     private void setArraySize(String input) {
         try {
             arraySize = Integer.parseInt(input);
@@ -222,6 +271,7 @@ public class StackEmulator extends Application {
         }
     }
 
+    // Function to push an element onto the stack
     private void pushElement(String input) {
         if (arraySize == 0) {
             displayMessage("Please set the array size first.");
@@ -241,6 +291,7 @@ public class StackEmulator extends Application {
         }
     }
 
+    // Function to pop an element from the stack
     private void popElement() {
         if (stack.isEmpty()) {
             displayMessage("Stack is empty. Nothing to pop.");
@@ -248,12 +299,13 @@ public class StackEmulator extends Application {
             // Apply fade-out animation to the top element before removing it
             StackBox topElement = (StackBox) stackBox.getChildren().get(stackBox.getChildren().size() - 1);
             applyFadeOutAnimation(topElement, () -> {
-                        stack.pop();
-                        updateStackView();
-                });
+                stack.pop();
+                updateStackView();
+            });
         }
     }
 
+    // Function to peek at the top element of the stack
     private void peekElement() {
         if (stack.isEmpty()) {
             displayMessage("Stack is empty.");
@@ -263,6 +315,7 @@ public class StackEmulator extends Application {
         }
     }
 
+    // Function to clear all elements from the stack
     private void clearStack() {
         if (stack.isEmpty()) {
             displayMessage("Stack is empty. Nothing to clear.");
@@ -272,11 +325,11 @@ public class StackEmulator extends Application {
         }
     }
 
+    // Function to reverse the order of elements in the stack
     private void reverseStack() {
         if (stack.isEmpty()) {
             displayMessage("Stack is empty. Nothing to reverse.");
-        } 
-        else{
+        } else {
             Stack<Integer> reversedStack = new Stack<>();
             while (!stack.isEmpty()) {
                 reversedStack.push(stack.pop());
@@ -286,6 +339,7 @@ public class StackEmulator extends Application {
         }
     }
 
+    // Function to display a message in an alert dialog
     private void displayMessage(String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Message");
@@ -294,6 +348,7 @@ public class StackEmulator extends Application {
         alert.showAndWait();
     }
 
+    // Function to update the visual representation of the stack
     private void updateStackView() {
         stackBox.getChildren().clear();
 
@@ -320,6 +375,7 @@ public class StackEmulator extends Application {
         }
     }
 
+    // Function to apply a bouncing animation to a stack element
     private void applyBounceAnimation(StackBox stackElement) {
         ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(0.5), stackElement);
         scaleTransition.setFromX(1);
@@ -331,6 +387,7 @@ public class StackEmulator extends Application {
         scaleTransition.play();
     }
 
+    // Function to apply a fade-in animation to a stack element
     private void applyFadeInAnimation(StackBox stackElement) {
         FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1), stackElement);
         fadeTransition.setFromValue(0);
@@ -338,6 +395,7 @@ public class StackEmulator extends Application {
         fadeTransition.play();
     }
 
+    // Function to apply a fade-out animation to a stack element
     private void applyFadeOutAnimation(StackBox stackElement, Runnable onFinish) {
         FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1), stackElement);
         fadeTransition.setFromValue(1);
@@ -346,6 +404,7 @@ public class StackEmulator extends Application {
         fadeTransition.play();
     }
 
+    // Custom StackPane class representing a stack element
     private class StackBox extends StackPane {
         public StackBox(String value) {
             setPrefSize(50, 50);
